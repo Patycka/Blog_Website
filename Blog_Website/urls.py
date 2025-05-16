b29851957b8b879ffffdc03297816b137bcb4f79
Your Name <you@example.com>
2025-05-16 16:42:20 +0200

    html and css files added with demo code

--- Blog_Website/urls.py
+++ Blog_Website/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')), #only one app that's why nor needed blog path
 ]
