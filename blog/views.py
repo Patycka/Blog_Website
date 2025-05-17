053b5a2d99482f5d8e7f88fdd2c6b2b9de91418a
Your Name <you@example.com>
2025-05-17 21:37:48 +0200

    Post detailed page html and css added.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def main_page(request):
@@ -9,4 +8,4 @@ def posts(request):
     return render(request, 'blog/all-posts.html')
 
 def show_post(request, slug):
-    return HttpResponse("First post")
+    return render(request, 'blog/post-detail.html')
